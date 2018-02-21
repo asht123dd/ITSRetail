a49562034c3e417f8f0bb2f6576bdafb1a8fe4cb
asht123dd <asht123dd@gmail.com>
2018-02-21 18:05:14 +0530

    Wheat done!!!

--- app/src/main/java/com/sdl/dart/itsretail/AllQuotesActivity.java
+++ app/src/main/java/com/sdl/dart/itsretail/AllQuotesActivity.java
@@ -2,6 +2,7 @@ package com.sdl.dart.itsretail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
@@ -10,19 +11,89 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class AllQuotesActivity extends AppCompatActivity {
     TextView price,qty;
+    ArrayList<Float> pr;
+    float f;
+    int i, j;
+    ArrayList<Integer> quantity;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mQuotesRef=mRootRef.child("quotes");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_quotes);
-        init();
+        i=0;
+        j=0;
+
+        pr=new ArrayList<Float>();
+        quantity=new ArrayList<>();
+        mQuotesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if(snapshot.child("commodity").getValue(String.class).equalsIgnoreCase("wheat"))
+                    {
+                        f = snapshot.child("price").getValue(Float.class);
+                        prAdd(f);
+                        Log.d("xyzr22", String.valueOf(pr.get(0)));
+                        // i++;
+                        quantity.add(snapshot.child("quantity").getValue(Integer.class));
+                        j++;
+                    }
+                   // TableRow row=new TableRow(this);
+                }
+                init();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+       Log.d("xyzr22","Value before init");
+        //init();
+
     }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        /*mQuotesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            f=snapshot.child("price").getValue(Float.class);
+                            prAdd(f);
+                           Log.d("xyzr22",String.valueOf(pr.get(0)));
+                           // i++;
+                            quantity.add(snapshot.child("quantity").getValue(Integer.class));
+                            j++;
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+        Log.d("xyzr22","Value before init");
+        init();*/
+    }
+    public void prAdd(float f)
+    {
+        pr.add(f);
+    }
+
     public void init(){
         TableLayout ll = (TableLayout) findViewById(R.id.tabLayout);
+        Log.d("xyzr22","Does this execute");
 
-
-        for (int i = 0; i <2; i++) {
+        for (int i = 0; i <pr.size(); i++) {
 
             TableRow row= new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
@@ -40,8 +111,8 @@ public class AllQuotesActivity extends AppCompatActivity {
             qty = new TextView(this);
            qty.setLayoutParams(param);
             qty.setGravity(Gravity.CENTER);
-            price.setText("hello");
-            qty.setText("10");
+            price.setText(String.format("%g",pr.get(i)));
+            qty.setText(String.valueOf(quantity.get(i)));
 
             row.addView(price);
             row.addView(qty);
