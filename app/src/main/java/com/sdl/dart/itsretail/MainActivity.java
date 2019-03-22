b1f9f39df9603419fe3966c21166cb7b11f4caab
asht123dd <asht123dd@gmail.com>
2019-03-22 19:44:11 +0530

    Card View Added

--- app/src/main/java/com/sdl/dart/itsretail/MainActivity.java
+++ app/src/main/java/com/sdl/dart/itsretail/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,10 +33,19 @@ public class MainActivity extends AppCompatActivity {
     JSONObject jArray = null;
     String text;
     private FirebaseAuth mAuth;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView =
+                (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
         mAuth = FirebaseAuth.getInstance();
        /* Rid=1;
         Log.v("xyzr22","getJSONcalled");
